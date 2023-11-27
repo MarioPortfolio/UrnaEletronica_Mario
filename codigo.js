010affd777e731a4b04173f7d214b5106d2deb62
MarioPortfolio <mariojr.github@gmail.com>
2023-11-27 13:44:32 -0300

    Criando Urna eletronica

---
+++ codigo.js
@@ -0,0 +1,8 @@
+function inserir(valor) {
+    var valor1 = document.getElementById("campo1").value
+    var valor2 = document.getElementById("campo2").value
+
+    if (valor1 == "") {
+        document.getElementById
+    }
+}
